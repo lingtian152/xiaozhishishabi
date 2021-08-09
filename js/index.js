e7b0158ac603dd1f3b324254a4f43c5b7b37871c
qw1324 <49418651+qw1324@users.noreply.github.com>
2021-08-09 17:40:07 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+var img = [
+    "../images/bg1.jpg",
+    "../images/bg2.jpg"
+];
+
+window.onload = function(){
+    var randomIndex = Math.floor(Math.random() * img.length)
+    document.getElementById("body").style.background = `url(${img[randomIndex]})`
+}
+
 document.getElementById("submit").onclick = function(){
     document.getElementById("announcement").remove();
 }
