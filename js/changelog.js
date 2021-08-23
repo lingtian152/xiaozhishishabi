adb8f4c1a1aa4419f5a9002d692d14c0163ceaf4
qw1324 <49418651+qw1324@users.noreply.github.com>
2021-08-23 18:47:08 -0400

    az

--- js/changelog.js
+++ js/changelog.js
@@ -1,9 +1,2 @@
 var changelogtext = document.getElementById("changelog-text");
 
-function readfile(path, text) {
-  changelogtext.innerHTML = text;
-}
-
-setTimeout(() => {
-  readfile("../changelog.txt", null);
-}, 2000);
