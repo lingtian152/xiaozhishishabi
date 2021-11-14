3c7eecab3ba597175fd21979c2ed58f2d42bc7e8
凌天 <49418651+qw1324@users.noreply.github.com>
2021-11-13 21:18:10 -0500

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,15 +1,13 @@
-
-
-// var img = [
-//     "../images/bg1.png",
-//     "../images/bg2.png",
-//     "../images/bg3.png",
-//     "../images/bg4.png",
-//     "../images/bg5.png",
-//     "../images/bg6.png",
-//     "../images/bg7.png",
-//     "../images/bg8.png"
-// ];
+var img = [
+    "../images/bg1.png",
+    "../images/bg2.png",
+    "../images/bg3.png",
+    "../images/bg4.png",
+    "../images/bg5.png",
+    "../images/bg6.png",
+    "../images/bg7.png",
+    "../images/bg8.png"
+];
 
 
 window.onload = function(){
