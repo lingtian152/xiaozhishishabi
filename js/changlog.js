6c1c316da34958b8dd5b01cc64ea1aefff01303d
qw1324 <49418651+qw1324@users.noreply.github.com>
2021-11-20 13:03:05 -0500

    Update changlog.js

--- js/changlog.js
+++ js/changlog.js
@@ -7,40 +7,33 @@
 // });
 
 function changeline(text) {
-    var htmls = [];
-    var lines = text.split(/\n/);
-    // The temporary <div/> is to perform HTML entity encoding reliably.
-    //
-    // document.createElement() is *much* faster than jQuery('<div></div>')
-    // http://stackoverflow.com/questions/268490/
-    //
-    // You don't need jQuery but then you need to struggle with browser
-    // differences in innerText/textContent yourself
-    var tmpDiv = jQuery(document.createElement('div'));
-    for (var i = 0 ; i < lines.length ; i++) {
-        htmls.push(tmpDiv.text(lines[i]).html());
-    }
-    return htmls.join("<br>");
+  var htmls = [];
+  var lines = text.split(/\n/);
+  // The temporary <div/> is to perform HTML entity encoding reliably.
+  //
+  // document.createElement() is *much* faster than jQuery('<div></div>')
+  // http://stackoverflow.com/questions/268490/
+  //
+  // You don't need jQuery but then you need to struggle with browser
+  // differences in innerText/textContent yourself
+  var tmpDiv = jQuery(document.createElement("div"));
+  for (var i = 0; i < lines.length; i++) {
+    htmls.push(tmpDiv.text(lines[i]).html());
+  }
+  return htmls.join("<br>");
 }
 
-function updatelog()
-{
-    $.ajax({
-        url: "../log.txt",
-        dataType: "text",
-        success: function (data) {
-          $("#changelog-txt").html(changeline(data));
-        },
-      });
+function loadlink() {
+  $.ajax({
+    url: "../log.txt",
+    dataType: "text",
+    success: function (data) {
+      $("#changelog-txt").load(changeline(data));
+    },
+  });
 }
 
-// function loadlink(){
-//     $('#changelog-txt').load('../log.txt',function () {
-//          $(this).unwrap();
-//     });
-// }
-
-updatelog(); // This will run on page load
-setInterval(function(){
-    updatelog() // this will run after every 5 seconds
+loadlink(); // This will run on page load
+setInterval(function () {
+  loadlink(); // this will run after every 5 seconds
 }, 5000);
