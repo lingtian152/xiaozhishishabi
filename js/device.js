a0736a1dd8ef2e03400f96f365f4f77660277d34
qw1324 <1553396053skwa@gmail.com>
2022-01-30 21:16:56 -0500

    update

--- js/device.js
+++ js/device.js
@@ -1,4 +1,20 @@
-if ($.browser.device == /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()))
-{
-  location.replace("m.xiaozhishishabi.tk");
+function iswap() {
+  var uA = navigator.userAgent.toLowerCase();
+  var ipad = uA.match(/ipad/i) == "ipad";
+  var iphone = uA.match(/iphone os/i) == "iphone os";
+  var midp = uA.match(/midp/i) == "midp";
+  var uc7 = uA.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+  var uc = uA.match(/ucweb/i) == "ucweb";
+  var android = uA.match(/android/i) == "android";
+  var windowsce = uA.match(/windows ce/i) == "windows ce";
+  var windowsmd = uA.match(/windows mobile/i) == "windows mobile";
+  if (!(ipad || iphone || midp || uc7 || uc || android || windowsce || windowsmd)) {
+    $("#body").css("background-image", `url(${"../images" + "/bg" + Math.floor(1 + Math.random() * 15) + ".png"})`);
+  } else {
+    $("#body").css("background-image", `url(${"../images/mobile/bg1.png"})`);
+  }
+}
+
+window.onload = function() {
+  iswap();
 }
--- js/image.js
+++ js/image.js
@@ -1,4 +1,4 @@
-var path = "../images";
+// var path = "../images";
 
-var randomIndex = path + "/bg" + Math.floor(1 + Math.random() * 15) + ".png";
-$("#body").css("background-image", `url(${randomIndex})`);
+// var randomIndex = path + "/bg" + Math.floor(1 + Math.random() * 15) + ".png";
+// $("#body").css("background-image", `url(${randomIndex})`);
